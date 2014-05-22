773ce5940c687c2edcd1a0aaabbeb36b753eb4d6
Callum Jones <callum@callumj.com>
2014-05-22 22:12:14 +0800

    Check no markup

--- src/org/opengraphtests/OpenGraphTest.java
+++ src/org/opengraphtests/OpenGraphTest.java
@@ -3,7 +3,7 @@ package org.opengraphtests;
 import org.junit.Test;
 import org.opengraph.OpenGraph;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class OpenGraphTest {
   @Test
@@ -11,5 +11,11 @@ public class OpenGraphTest {
     OpenGraph site = new OpenGraph("http://www.bbc.com/future/story/20140428-the-myth-of-tech-revolutions", true);
     assertEquals("Why it’s time to ditch the word ‘revolution’ in tech", site.getContent("title"));
     assertEquals("624", site.getContent("image:width"));
+  }
+
+  @Test
+  public void shouldNoOGMarkup() throws java.lang.Exception {
+    OpenGraph site = new OpenGraph("http://clang.llvm.org/docs/UsersManual.html", true);
+    assertNull(site.getContent("title"));
   }
 } 
