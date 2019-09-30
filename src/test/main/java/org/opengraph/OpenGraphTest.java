419a86f9985e7fb24b9af89f75f45fc7ac444a71
Dong <yidonguk@gmail.com>
2019-09-30 14:35:53 +0900

    fix of erata

--- src/test/main/java/org/opengraph/OpenGraphTest.java
+++ src/test/main/java/org/opengraph/OpenGraphTest.java
@@ -9,7 +9,7 @@ public class OpenGraphTest {
   @Test
   public void shouldHandleMissingContentType() throws java.lang.Exception {
     OpenGraph site = new OpenGraph("http://www.bbc.com/future/story/20140428-the-myth-of-tech-revolutions", true);
-    assertEquals("Why it�s time to ditch the word �revolution� in tech", site.getContent("title"));
+    assertEquals("Why it’s time to ditch the word ‘revolution’ in tech", site.getContent("title"));
     assertEquals("624", site.getContent("image:width"));
   }
 
