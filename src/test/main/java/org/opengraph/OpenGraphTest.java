bd66a42db0756409663adc3b94d9c74cb3186816
Dong <yidonguk@gmail.com>
2019-09-30 14:13:35 +0900

    support of HTTP 30X redirect

--- src/test/main/java/org/opengraph/OpenGraphTest.java
+++ src/test/main/java/org/opengraph/OpenGraphTest.java
@@ -9,7 +9,7 @@ public class OpenGraphTest {
   @Test
   public void shouldHandleMissingContentType() throws java.lang.Exception {
     OpenGraph site = new OpenGraph("http://www.bbc.com/future/story/20140428-the-myth-of-tech-revolutions", true);
-    assertEquals("Why it’s time to ditch the word ‘revolution’ in tech", site.getContent("title"));
+    assertEquals("Why itï¿½s time to ditch the word ï¿½revolutionï¿½ in tech", site.getContent("title"));
     assertEquals("624", site.getContent("image:width"));
   }
 
@@ -17,5 +17,13 @@ public class OpenGraphTest {
   public void shouldNoOGMarkup() throws java.lang.Exception {
     OpenGraph site = new OpenGraph("http://clang.llvm.org/docs/UsersManual.html", true);
     assertNull(site.getContent("title"));
+  }
+  @Test
+  public void testRedirect() throws java.lang.Exception {
+    OpenGraph site = new OpenGraph("http://www.naver.com", true); // this url should be redirected to https scheme.
+    assertNotNull(site.getContent("title"));
+    
+    site = new OpenGraph("https://bit.ly/2maMYF0", true);
+    assertNotNull(site.getContent("title"));
   }
 } 
