2a28be2afbd417806bb566d15b57ba4f3b42604b
Timothy Stone <github@petmystone.com>
2021-05-09 13:44:23 -0400

    Fix tests using BBC
    BBC reorganized URIs and moved to HTTPS

--- src/test/java/org/opengraph/OpenGraphTest.java
+++ src/test/java/org/opengraph/OpenGraphTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class OpenGraphTest {
   @Test
   public void shouldHandleMissingContentType() throws java.lang.Exception {
-    OpenGraph site = new OpenGraph("http://www.bbc.com/future/story/20140428-the-myth-of-tech-revolutions", true);
+    OpenGraph site = new OpenGraph("https://www.bbc.com/future/article/20140428-the-myth-of-tech-revolutions", true);
     assertEquals("Why it’s time to ditch the word ‘revolution’ in tech", site.getContent("title"));
     assertEquals("624", site.getContent("image:width"));
   }
