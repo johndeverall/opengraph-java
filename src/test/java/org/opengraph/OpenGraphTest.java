92c0875bc24107665691a7bdccef64df6b6b8d2c
ch-li <smling9_9@hotmail.com>
2024-03-09 20:16:46 +0000

    update package.

--- src/test/java/org/opengraph/OpenGraphTest.java
+++ src/test/java/org/opengraph/OpenGraphTest.java
@@ -1,21 +1,22 @@
 package org.opengraph;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.opengraph.OpenGraph;
 
-import static org.junit.Assert.*;
 
 public class OpenGraphTest {
   @Test
   public void shouldHandleMissingContentType() throws java.lang.Exception {
     OpenGraph site = new OpenGraph("http://www.bbc.com/future/story/20140428-the-myth-of-tech-revolutions", true);
-    assertEquals("Why it’s time to ditch the word ‘revolution’ in tech", site.getContent("title"));
-    assertEquals("624", site.getContent("image:width"));
+    Assertions.assertEquals("Why itï¿½s time to ditch the word ï¿½revolutionï¿½ in tech", site.getContent("title"));
+    Assertions.assertEquals("624", site.getContent("image:width"));
   }
 
   @Test
   public void shouldNoOGMarkup() throws java.lang.Exception {
     OpenGraph site = new OpenGraph("http://clang.llvm.org/docs/UsersManual.html", true);
-    assertNull(site.getContent("title"));
+    Assertions.assertNull(site.getContent("title"));
   }
 } 
